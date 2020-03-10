0c8e0c8675a606e18e29976614e16ddd61bafe90
Isak Kallenbach <kallenbach13@gmail.com>
2020-03-10 23:15:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,4 +1,5 @@
 class Dog
+<<<<<<< HEAD
     def initialize
       @name = name
       @breed = breed
@@ -20,3 +21,26 @@ class Dog
       @breed = new_breed
     end
   end
+=======
+  def initialize
+    @name = name
+    @breed = breed
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+end
+>>>>>>> 10964c831d229a70b76f32c995ebfb2d73457159
--- lib/person.rb
+++ lib/person.rb
@@ -1,4 +1,5 @@
 class Person
+<<<<<<< HEAD
     def initialize
       @name = name
       @job = job
@@ -20,3 +21,26 @@ class Person
       @job = new_job
     end
   end
+=======
+  def initialize
+    @name = name
+    @job = job
+  end
+  
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def job
+    @job
+  end
+  
+  def job=(new_job)
+    @job = new_job
+  end
+end
+>>>>>>> 10964c831d229a70b76f32c995ebfb2d73457159
